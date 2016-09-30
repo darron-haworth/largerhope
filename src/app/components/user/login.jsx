d53e2866c557c9180b9d0af416f9e98e779cc4d5
Darron Haworth <darron.haworth@firsttechfed.com>
2016-09-30 13:01:35 -0700

    remove unused auth providers

--- src/app/components/user/login.jsx
+++ src/app/components/user/login.jsx
@@ -73,19 +73,6 @@ class UserLogin extends Component {
           <a href="#" className="btn btn-block btn-social btn-facebook" onClick={() => {
             this.loginWithProvider("facebook")
           } } data-provider="facebook">Facebook</a>
-
-          <a href="#" className="btn btn-block btn-social btn-twitter" onClick={() => {
-            this.loginWithProvider("twitter")
-          } } data-provider="twitter">Twitter</a>
-
-          <a href="#" className="btn btn-block btn-social btn-google" onClick={() => {
-            this.loginWithProvider("google")
-          } } data-provider="twitter">Google</a>
-
-           <a href="#" className="btn btn-block btn-social btn-github" onClick={() => {
-            this.loginWithProvider("github")
-          } } data-provider="twitter">Github</a>
-
         </form>
       </div>
 
