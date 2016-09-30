897068a122914f5c6c73fcd642d4bd10d542ba63
Darron Haworth <darron.haworth@firsttechfed.com>
2016-09-30 12:45:14 -0700

    init with my largerhope firebase

---
+++ src/app/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  // Change this to your firebase configuration! (Add Firebase to your web app)
+  FIREBASE_CONFIG: {
+
+    apiKey: "AIzaSyCtYDyUPV-bCqAHx1JlMzOzxtAihBJAMic",
+    authDomain: "largerhope-2a024.firebaseapp.com",
+    databaseURL: "https://largerhope-2a024.firebaseio.com",
+    storageBucket: "largerhope-2a024.appspot.com",
+    messagingSenderId: "960141279394"
+
+  }
+};
