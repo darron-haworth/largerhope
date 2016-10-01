33ab1027a7936f1155f28256022786c9b0986d8e
Darron Haworth <darron.haworth@firsttechfed.com>
2016-10-01 10:30:01 -0700

    using copy webpack plugin npm to copy static resources to dist folder

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,6 +2,7 @@ const Path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (options) => {
   const ExtractSASS = new ExtractTextPlugin(`/styles/${options.cssFileName}`);
@@ -16,19 +17,20 @@ module.exports = (options) => {
     output: {
       path: Path.join(__dirname, '../dist'),
       filename: `/scripts/${options.jsFileName}`,
+
     },
     resolve: {
       extensions: ['', '.js', '.jsx'],
     },
     module: {
       loaders: [
-        {test: /.jsx?$/, include: Path.join(__dirname, '../src/app'), loader: 'babel',},
-        {test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loader: 'babel'},
-        {test: /\.css$/, loader: 'style-loader!css-loader'},
-        {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
-        {test: /\.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000"},
-        {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
-        {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"}
+        { test: /.jsx?$/, include: Path.join(__dirname, '../src/app'), loader: 'babel', },
+        { test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loader: 'babel' },
+        { test: /\.css$/, loader: 'style-loader!css-loader' },
+        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+        { test: /\.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000" },
+        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
       ],
     },
     plugins: [
@@ -40,6 +42,9 @@ module.exports = (options) => {
       new HtmlWebpackPlugin({
         template: Path.join(__dirname, '../src/index.html'),
       }),
+      new CopyWebpackPlugin([
+            { from: 'src/static', to: 'static' }
+        ]),
     ],
   };
 
